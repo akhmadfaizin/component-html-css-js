06ca82c161092c78275d5a4218ffe996a28c6197
Akhmad Faizin <akhmadfragment@gmail.com>
2025-07-10 14:58:28 +0800

    w3s - Menu Icon

--- src/w3sComponent/IconBar/script.js
+++ src/w3sComponent/IconBar/script.js
@@ -1,12 +1,16 @@
 function toggleSidebar() {
-  const sidebar = document.querySelector('.sidebar');
-  const toggleIcon = document.querySelector('.toggle-btn i');
-  sidebar.classList.toggle('compact');
-  if (sidebar.classList.contains('compact')) {
-    toggleIcon.classList.remove('fa-chevron-right');
-    toggleIcon.classList.add('fa-chevron-left');
+  const sidebar = document.querySelector(".sidebar");
+  const toggleIcon = document.querySelector(".toggle-btn i");
+  sidebar.classList.toggle("compact");
+  if (sidebar.classList.contains("compact")) {
+    toggleIcon.classList.remove("fa-chevron-right");
+    toggleIcon.classList.add("fa-chevron-left");
   } else {
-    toggleIcon.classList.remove('fa-chevron-left');
-    toggleIcon.classList.add('fa-chevron-right');
+    toggleIcon.classList.remove("fa-chevron-left");
+    toggleIcon.classList.add("fa-chevron-right");
   }
+}
+
+function myFunction(x) {
+  x.classList.toggle("change");
 }
